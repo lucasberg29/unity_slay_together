73f7c422c43b0e757ed983910eecfa2c2a2a8ff3
lucasberg29 <lucasberg29@gmail.com>
2025-08-13 22:37:32 -0400

    added trigger controls for menu

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -1,16 +1,112 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
+using UnityEditor.ProBuilder;
+using UnityEditor.ShaderKeywordFilter;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
+    private bool inMenu;
+    private MenuSection currentSection;
 
-    public void PlayLevel(string levelTag)
+    private ControllerHandler controllerHandler;
+
+    private float currentLeftTriggerDepth = 0.0f;
+    private float currentRightTriggerDepth = 0.0f;
+
+    public GameObject[] menuSections;
+    private int currentIndex;
+
+    public float menuSectionSwitchCooldown = 1.0f;
+    private float menuSectionSwitchCooldownTimer = 0.0f;
+
+    private void Start()
     {
-        GameManager._instance.SetCurrentLevel(levelTag);
-        SceneManager.LoadScene(levelTag);
+        currentIndex = 0;
+        currentSection = MenuSection.Game;
+        controllerHandler = GameObject.FindGameObjectWithTag("Player").GetComponent<ControllerHandler>();
     }
 
+    private void Update()
+    {
+        if (menuSectionSwitchCooldownTimer <= 0)
+        {
+            UpdateLeftTrigger();
+            UpdateRightTrigger();
+        }
+        else
+        {
+            menuSectionSwitchCooldownTimer -= Time.deltaTime;
+            currentLeftTriggerDepth = 0.0f;
+            currentRightTriggerDepth = 0.0f;
+        }
+    }
 
+    private void UpdateRightTrigger()
+    {
+        float rightTriggerDepth = controllerHandler.rightTriggerDepth;
+        if (rightTriggerDepth >= 0.5f && currentRightTriggerDepth < rightTriggerDepth)
+        {
+            MoveRightInMenu();
+            currentRightTriggerDepth = rightTriggerDepth;
+            menuSectionSwitchCooldownTimer = menuSectionSwitchCooldown; 
+        }
+    }
+
+    private void UpdateLeftTrigger()
+    {
+        float leftTriggerDepth = controllerHandler.leftTriggerDepth;
+        if (leftTriggerDepth >= 0.5f && currentLeftTriggerDepth < leftTriggerDepth)
+        {
+            MoveLeftInMenu();
+            currentLeftTriggerDepth = leftTriggerDepth;
+            menuSectionSwitchCooldownTimer = menuSectionSwitchCooldown;
+        }
+    }
+
+    private void MoveLeftInMenu()
+    {
+        currentIndex--;
+
+        if (currentIndex < 0)
+        {
+            currentIndex = menuSections.Length - 1;
+        }
+
+        for (int i = 0; i < menuSections.Length; i++)
+        {
+            menuSections[i].SetActive(false);
+        }
+
+        menuSections[currentIndex].SetActive(true); 
+    }
+
+    private void MoveRightInMenu()
+    {
+        currentIndex++;
+
+        if (currentIndex > menuSections.Length - 1)
+        {
+            currentIndex = 0;
+        }
+
+        for (int i = 0; i < menuSections.Length; i++)
+        {
+            menuSections[i].SetActive(false);
+        }
+
+        menuSections[currentIndex].SetActive(true);
+    }
+}
+
+public enum MenuSection
+{
+    Game,
+    Audio,
+    Video,
+    Credits
 }
